1fff1523645f7da0c0aebe98ea6b1d742bf02ed5
Sarah Pak <suji.pak93@gmail.com>
2021-07-12 03:23:37 -0400

    added xla import and changed quotes for distrib.py

--- denoiser/distrib.py
+++ denoiser/distrib.py
@@ -9,6 +9,8 @@ import logging
 import os
 
 import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader, Subset
 from torch.nn.parallel.distributed import DistributedDataParallel
@@ -33,13 +35,14 @@ def init(args):
     torch.cuda.set_device(rank)
     torch.distributed.init_process_group(
         backend=args.ddp_backend,
-        init_method='file://' + os.path.abspath(args.rendezvous_file),
+        init_method="file://" + os.path.abspath(args.rendezvous_file),
         world_size=world_size,
-        rank=rank)
+        rank=rank,
+    )
     logger.debug("Distributed rendezvous went well, rank %d/%d", rank, world_size)
 
 
-def average(metrics, count=1.):
+def average(metrics, count=1.0):
     """average.
 
     Average all the relevant metrices across processes
@@ -48,7 +51,7 @@ def average(metrics, count=1.):
     """
     if world_size == 1:
         return metrics
-    tensor = torch.tensor(list(metrics) + [1], device='cuda', dtype=torch.float32)
+    tensor = torch.tensor(list(metrics) + [1], device="cuda", dtype=torch.float32)
     tensor *= count
     torch.distributed.all_reduce(tensor, op=torch.distributed.ReduceOp.SUM)
     return (tensor[:-1] / tensor[-1]).cpu().numpy().tolist()
@@ -65,7 +68,8 @@ def wrap(model):
         return DistributedDataParallel(
             model,
             device_ids=[torch.cuda.current_device()],
-            output_device=torch.cuda.current_device())
+            output_device=torch.cuda.current_device(),
+        )
 
 
 def barrier():
